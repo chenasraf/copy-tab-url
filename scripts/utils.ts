0d0a9bf63b57943f924008cd92c7b4df72ff5c2a
Chen Asraf <casraf@pm.me>
2024-10-04 03:11:54 +0300

    feat: initial commit

---
+++ scripts/utils.ts
@@ -0,0 +1,16 @@
+import { resolve } from 'path'
+import { bgCyan, black } from 'kolorist'
+import react from '@vitejs/plugin-react'
+const __dirname = import.meta.dirname
+
+export const fastRefresh = true
+
+export const port = parseInt(process.env.PORT || '') || 3303
+export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
+export const isDev = process.env.NODE_ENV !== 'production'
+export const preambleCode = react.preambleCode.replace('__BASE__', '/')
+
+export function log(name: string, message: string) {
+  // eslint-disable-next-line no-console
+  console.log(black(bgCyan(` ${name} `)), message)
+}
